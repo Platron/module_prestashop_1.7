bddb8cbfe4786e88938c588f26e5ad01421b2981
lashnag <lexey2006.90@mail.ru>
2019-01-09 16:59:24 +0300

    Добавлен тип позиции

--- platron/OfdReceiptItem.php
+++ platron/OfdReceiptItem.php
@@ -6,6 +6,7 @@ class OfdReceiptItem
     public $price;
     public $quantity;
     public $vat;
+    public $type = 'product';
     
     public function getAttributeAsArray()
     {
@@ -15,6 +16,7 @@ class OfdReceiptItem
             'pg_price' => $this->price,
             'pg_quantity' => $this->quantity,
             'pg_vat' => $this->vat,
+            'pg_type' => $this->type,
         );
     }
 }
